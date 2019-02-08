4b076a3218bbb15c7e003f3e1306cddc4f4588a9
Sean Leclerc <47339189+SeanLeclerc@users.noreply.github.com>
2019-02-07 21:03:37 -0500

    Add files via upload

---
+++ Exercice022/Exercice022/Form1.Designer.vb
@@ -0,0 +1,136 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ListView2 = New System.Windows.Forms.ListView()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Label26 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ListView2
+        '
+        Me.ListView2.Location = New System.Drawing.Point(12, 12)
+        Me.ListView2.Name = "ListView2"
+        Me.ListView2.Size = New System.Drawing.Size(653, 515)
+        Me.ListView2.TabIndex = 360
+        Me.ListView2.UseCompatibleStateImageBehavior = False
+        '
+        'Button4
+        '
+        Me.Button4.Enabled = False
+        Me.Button4.Location = New System.Drawing.Point(384, 552)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(281, 53)
+        Me.Button4.TabIndex = 359
+        Me.Button4.Text = "La collection est automatiquement chargée lors du chargement du formulaire - Reli" &
+    "re fichier format délmité(|) et Recréer la collection"
+        Me.Button4.UseVisualStyleBackColor = True
+        Me.Button4.Visible = False
+        '
+        'Label26
+        '
+        Me.Label26.Font = New System.Drawing.Font("Arial Black", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label26.Location = New System.Drawing.Point(21, 560)
+        Me.Label26.Name = "Label26"
+        Me.Label26.Size = New System.Drawing.Size(311, 21)
+        Me.Label26.TabIndex = 358
+        Me.Label26.Text = "Sélectionner la clé dans la collection"
+        Me.Label26.Visible = False
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(24, 584)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(318, 21)
+        Me.ComboBox1.TabIndex = 357
+        Me.ComboBox1.Visible = False
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(688, 493)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 364
+        Me.Button5.Text = "Fermer"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(688, 70)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(75, 23)
+        Me.Button6.TabIndex = 367
+        Me.Button6.Text = "Petite icône"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(688, 41)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(75, 23)
+        Me.Button7.TabIndex = 366
+        Me.Button7.Text = "Liste"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(688, 12)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(75, 23)
+        Me.Button8.TabIndex = 365
+        Me.Button8.Text = "Détails"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(912, 545)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.ListView2)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Label26)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents ListView2 As System.Windows.Forms.ListView
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Label26 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+End Class
---
+++ Exercice022/Exercice022/Form1.vb
@@ -0,0 +1,203 @@
+﻿Public Class Form1
+    Public Champs As New ArrayList()
+
+    Public Structure ProprioVehicules
+        Dim Province As String
+        Dim ville As String
+        Dim NoMembre As Integer
+        Dim Nom As String
+        Dim Prenom As String
+        Dim Adresse As String
+        Dim CodePostal As String
+        Dim Notelephone As String
+        Dim Courriel As String
+        Dim Langue As String
+        Dim DateNaissance As String
+        Dim Marque As String
+        Dim Modele As String
+    End Structure
+
+    'Avec la collection SortedList, les clés sont triées automatiquement
+    'Public FichePays As New SortedList(Of Integer, ProprioVehicules)
+    Public FichePays As New SortedList(Of String, ProprioVehicules)
+
+    Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+        Dim NodeKey As String = ""
+        Dim categorieMarque As String = ""
+        Dim PreviousCategorieMarque As String = ""
+        Dim ItemMembre As String = ""
+
+        Button4_Click(sender, e)
+
+        For Each NodeKey In FichePays.Keys
+            'Extrire données de la collection et stocker données dans la structure
+            Dim DonneesList As New ProprioVehicules
+            DonneesList.Province = FichePays(NodeKey).Province
+            DonneesList.ville = FichePays(NodeKey).ville
+            DonneesList.NoMembre = FichePays(NodeKey).NoMembre
+            DonneesList.Nom = FichePays(NodeKey).Nom
+            DonneesList.Prenom = FichePays(NodeKey).Prenom
+            DonneesList.Adresse = FichePays(NodeKey).Adresse
+            DonneesList.Province = FichePays(NodeKey).Province
+            DonneesList.CodePostal = FichePays(NodeKey).CodePostal
+            DonneesList.Notelephone = FichePays(NodeKey).Notelephone
+            DonneesList.Courriel = FichePays(NodeKey).Courriel
+            DonneesList.Langue = FichePays(NodeKey).Langue
+            DonneesList.DateNaissance = FichePays(NodeKey).DateNaissance
+            DonneesList.Modele = FichePays(NodeKey).Modele
+
+
+            DecoderNodeKey(NodeKey)
+            Dim nCategorie As New ListViewGroup
+            Dim nMembre As New ListViewItem
+            categorieMarque = Champs(0)
+            ItemMembre = CStr(Champs(1))
+
+            ListView2.Columns.Clear()
+            ListView2.Columns.Add("No", 25, HorizontalAlignment.Left)
+            ListView2.Columns.Add("Nom", 80, HorizontalAlignment.Left)
+            ListView2.Columns.Add("Prénom", 60, HorizontalAlignment.Left)
+            ListView2.Columns.Add("Adresse", 60, HorizontalAlignment.Left)
+            ListView2.Columns.Add("Ville", 60, HorizontalAlignment.Left)
+            ListView2.Columns.Add("Province", 60, HorizontalAlignment.Left)
+            ListView2.Columns.Add("Code Postal", 60, HorizontalAlignment.Left)
+            ListView2.Columns.Add("No Telephone", 60, HorizontalAlignment.Left)
+            ListView2.Columns.Add("Courriel", 60, HorizontalAlignment.Left)
+            ListView2.Columns.Add("Langue", 60, HorizontalAlignment.Left)
+            ListView2.Columns.Add("Date naissance", 60, HorizontalAlignment.Left)
+            ListView2.Columns.Add("modele", 60, HorizontalAlignment.Left)
+
+            If categorieMarque <> PreviousCategorieMarque Then
+                nCategorie.Header = categorieMarque
+                nCategorie.Name = categorieMarque
+                ListView2.Groups.Add(nCategorie)
+                nMembre.Text = ItemMembre
+                nMembre.Tag = NodeKey
+                nMembre.Group = ListView2.Groups(categorieMarque)
+                nMembre.SubItems.Add(DonneesList.Nom)
+                nMembre.SubItems.Add(DonneesList.Prenom)
+                nMembre.SubItems.Add(DonneesList.Adresse)
+                nMembre.SubItems.Add(DonneesList.ville)
+                nMembre.SubItems.Add(DonneesList.Province)
+                nMembre.SubItems.Add(DonneesList.CodePostal)
+                nMembre.SubItems.Add(DonneesList.Notelephone)
+                nMembre.SubItems.Add(DonneesList.Courriel)
+                nMembre.SubItems.Add(DonneesList.Langue)
+                nMembre.SubItems.Add(DonneesList.DateNaissance)
+                nMembre.SubItems.Add(DonneesList.Modele)
+                ListView2.Items.Add(nMembre)
+                PreviousCategorieMarque = categorieMarque
+
+            Else
+                nMembre.Text = ItemMembre
+                nMembre.Tag = NodeKey
+                nMembre.Group = ListView2.Groups(categorieMarque)
+                nMembre.SubItems.Add(DonneesList.Nom)
+                nMembre.SubItems.Add(DonneesList.Prenom)
+                nMembre.SubItems.Add(DonneesList.Adresse)
+                nMembre.SubItems.Add(DonneesList.ville)
+                nMembre.SubItems.Add(DonneesList.Province)
+                nMembre.SubItems.Add(DonneesList.CodePostal)
+                nMembre.SubItems.Add(DonneesList.Notelephone)
+                nMembre.SubItems.Add(DonneesList.Courriel)
+                nMembre.SubItems.Add(DonneesList.Langue)
+                nMembre.SubItems.Add(DonneesList.DateNaissance)
+                nMembre.SubItems.Add(DonneesList.Modele)
+                ListView2.Items.Add(nMembre)
+            End If
+        Next NodeKey
+        ListView2.View = View.Details
+        ListView2.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent)
+        ListView2.GridLines = True
+        ListView2.FullRowSelect = True
+        ListView2.LabelEdit = True
+        ListView2.MultiSelect = True
+        ListView2.Sorting = SortOrder.Ascending
+
+    End Sub
+    Public Sub DecoderNodeKey(ByVal keynode As String)
+        Champs.Clear()
+        Dim positionDelimiteur As Integer
+        Do
+            positionDelimiteur = InStr(keynode, "\")
+            Champs.Add(Microsoft.VisualBasic.Left(keynode, positionDelimiteur - 1))
+            keynode = Microsoft.VisualBasic.Right(keynode, Len(keynode) - positionDelimiteur)
+        Loop Until InStr(keynode, "\") = 0
+        Champs.Add(keynode)
+    End Sub
+
+    Public Sub ChargerDansCollection()
+        Dim cle As String
+        Dim Pays As New ProprioVehicules
+        cle = Champs(11) & "\" & CStr(Champs(2))
+        Pays.Province = Champs(0)
+        Pays.ville = Champs(1)
+        Pays.NoMembre = Champs(2)
+        Pays.Nom = Champs(3)
+        Pays.Prenom = Champs(4)
+        Pays.Adresse = Champs(5)
+        Pays.CodePostal = Champs(6)
+        Pays.Notelephone = Champs(7)
+        Pays.Courriel = Champs(8)
+        Pays.Langue = Champs(9)
+        Pays.DateNaissance = Champs(10)
+        Pays.Marque = Champs(11)
+        Pays.Modele = Champs(12)
+
+        If Not FichePays.ContainsKey(cle) Then
+            FichePays.Add(cle, Pays)
+        End If
+    End Sub
+    Private Sub ExtraireChampsDunEnregistrementFichierdelimite(ByVal chaine As String)
+        Champs.Clear()
+        Dim positionDelimiteur As Integer
+        Do
+            positionDelimiteur = InStr(chaine, "|")
+            Champs.Add(Microsoft.VisualBasic.Left(chaine, positionDelimiteur - 1))
+            chaine = Microsoft.VisualBasic.Right(chaine, Len(chaine) - positionDelimiteur)
+        Loop Until InStr(chaine, "|") = 0
+        Champs.Add(chaine)
+    End Sub
+
+    Public Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        Dim lineNo As Integer = 0
+        Dim fileReader As System.IO.StreamReader
+        Dim stringReader As String
+        fileReader = My.Computer.FileSystem.OpenTextFileReader("..\..\..\Exercice022.txt")
+        While Not fileReader.EndOfStream
+            stringReader = fileReader.ReadLine()
+            lineNo = lineNo + 1
+            'MsgBox(CStr(lineNo) & " : " & stringReader)
+            ExtraireChampsDunEnregistrementFichierdelimite(stringReader)
+            ChargerDansCollection()
+        End While
+        fileReader.Close()
+    End Sub
+
+
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        'Afficher la vue détails
+        ListView2.View = View.Details
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        'Afficher la vue  liste
+        ListView2.View = View.List
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        'Afficher la vue avec des petites icönes
+        ListView2.View = View.SmallIcon
+    End Sub
+
+    Private Sub ListView2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListView2.SelectedIndexChanged
+
+    End Sub
+End Class
+
+
